9d1e602417dbe42fe50083cf67a7b09602ff1ad8
Brenna Pacheco <brennapacheco@hotmail.com>
2022-11-04 15:37:00 -0300

    Add start game method

--- WordScramble/WordScramble/Screens/ViewController.swift
+++ WordScramble/WordScramble/Screens/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var allWords = [String]()
+    var usedWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") { //acesso o arquivo dentro do projeto
+            if let startWords = try? String(contentsOf: startWordsURL) { //converto pra string o conteudo do arquivo
+                allWords = startWords.components(separatedBy: "\n") //atribuo ao array/lista as palavras separadas por um breakline
+            }
+        }
+
+        if allWords.isEmpty {
+            allWords = ["silkworm"] //adiciono uma palavra aleatoria
+        }
+        startGame()
     }
 
+    func loadFiles() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") { //acesso o arquivo dentro do projeto
+            if let startWords = try? String(contentsOf: startWordsURL) { //converto pra string o conteudo do arquivo
+                allWords = startWords.components(separatedBy: "\n") //atribuo ao array/lista as palavras separadas por um breakline
+            }
+        }
 
+        if allWords.isEmpty {
+            allWords = ["silkworm"] //adiciono uma palavra aleatoria
+        }
+    }
+    
+    func startGame() {
+        title = allWords.randomElement() //titulo da pagina com uma palavra aleatoria do array
+        usedWords.removeAll(keepingCapacity: true) //remove todos os elementos do array
+        tableView.reloadData() //atualiza os dados da tabela
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 }
 
